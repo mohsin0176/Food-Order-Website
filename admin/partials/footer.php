f8e0022f6f85f7b89a03b0b51e248cb3d3b72731
mohsin0176 <mohsin2010ju@gmail.com>
2021-03-25 11:16:28 -0700

    Restaurant Template created

---
+++ admin/partials/footer.php
@@ -0,0 +1,7 @@
+<div class="footer">
+	<div class="wrapper">
+	<p>2020 All rights reserved,Some Restaurant developed By INK</p>
+</div>
+
+</body>
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Food Oreder Website -- Home Page</title>
+<link rel="stylesheet" type="text/css" href="../css/admin.css">
+</head>
+<body>
+<div class="menu text-center">
+	<div class="wrapper">
+	<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="manage-admin.php">Admin</a></li>
+	<li><a href="manage-category.php">Category</a></li>
+	<li><a href="manage-food.php">Food</a></li>
+	<li><a href="manage-order.php">Order</a></li>
+	</ul>
+	</div>
+</div>
